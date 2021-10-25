c019a8f9c4cf7c618c6222957b2865ea76019196
Febriyaninurhida123 <sutarmiprihatna@gmail.com>
2021-10-25 22:51:52 +0700

    menambahkan project

---
+++ latihan1_membuat_biodata.py
@@ -0,0 +1,17 @@
+# Mengambil input
+Nama = input("Nama lengkap:")
+Namapanggilan = input("Nama panggilan:")
+NPM = input("NPM:")
+tempatlahir = input("Tempat lahir:")
+umur = input("Umur:")
+alamat = input("alamat:")
+telepon = input("telepon:")
+
+# Menapilkan output
+print("Assalamualaikum")
+
+
+print("Let me introduce myself. My name is",Nama, "but you can call me",Namapanggilan,"My NPM is",NPM,". I was born in",tempatlahir,"and i am very glad if you want to invite my house in",alamat,". So don't forget to call me before with the number.",telepon)
+
+
+print("Thank you")
